06f469357583f49d9a19690d494b9515c6d364aa
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-16 13:46:39 +0600

    Revert "Добавлен функционал распределения по группам пользователем"
    This reverts commit 26b2c7085a1c63c6a40ca12a1b188cdf9e708dd6.

--- headhunter/accounts/forms.py
+++ headhunter/accounts/forms.py
@@ -50,11 +50,8 @@ class CustomUserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data.get('password'))
-        user.save()
-        if user.is_employer:
-            user.groups.add(1)
-        user.groups.add(2)
-        user.save()
+        if commit:
+            user.save()
         return user
 
 
