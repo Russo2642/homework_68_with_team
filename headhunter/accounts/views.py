98d34d674233312b8fd02d4ad697c0aef5b7e805
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-17 01:51:33 +0600

    #5 Личный кабинет пользователя. Редактирование личных данных
    Попытка создания функционала редактирования личных данных в модальном окне

--- headhunter/accounts/views.py
+++ headhunter/accounts/views.py
@@ -4,7 +4,9 @@ from accounts.forms import UserChangeForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.shortcuts import redirect, get_object_or_404
+from django.template.loader import render_to_string
 from django.urls import reverse
 from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 
@@ -33,7 +35,7 @@ class LoginView(TemplateView):
         next = request.GET.get('next')
         if next:
             return redirect(next)
-        return redirect('index')
+        return redirect('/')
 
 
 def logout_view(request):
@@ -62,14 +64,43 @@ class ProfileView(LoginRequiredMixin, DetailView):
     context_object_name = 'user_obj'
 
 
-class UserChangeView(UserPassesTestMixin, UpdateView):
-    model = get_user_model()
-    form_class = UserChangeForm
-    template_name = 'user_change.html'
-    context_object_name = 'user_obj'
+def save_book_form(request, form, template_name):
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            user = get_user_model()
+            data['html_book_list'] = render_to_string('user_details.html', {
+                'user': user
+            })
+        else:
+            data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
 
-    def get_success_url(self):
-        return reverse('profile', kwargs={'pk': self.object.pk})
 
-    def test_func(self):
-        return self.get_object() == self.request.user
+# class UserChangeView(UserPassesTestMixin, UpdateView):
+#     model = get_user_model()
+#     form_class = UserChangeForm
+#     template_name = 'user_change.html'
+#     context_object_name = 'user_obj'
+#
+#     def post(self, request, *args, **kwargs):
+#         user = get_user_model()
+#         form = UserChangeForm(request.POST, instance=user)
+#         return save_book_form(request, form, self.template_name)
+#
+#     def get_success_url(self):
+#         return reverse('profile', kwargs={'pk': self.object.pk})
+#
+#     def test_func(self):
+#         return self.get_object() == self.request.user
+def user_change_view(request, pk):
+    user = get_object_or_404(get_user_model(), pk=pk)
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=user)
+    else:
+        form = UserChangeForm(instance=user)
+    return save_book_form(request, form, 'user_change.html')
