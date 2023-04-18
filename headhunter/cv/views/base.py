5e289664c44bb9480943c8b76eb02e6f734a5f4b
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-18 17:59:47 +0600

    #9 Личный кабинет пользователя. Публикация резюме
    Добавил вёрстку

--- headhunter/cv/views/base.py
+++ headhunter/cv/views/base.py
@@ -1,5 +1,16 @@
-from django.views.generic import TemplateView
+from cv.models import CV
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Permission
+from django.db.models import Q, Max
+from django.shortcuts import render
+from django.views import View
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class IndexView(LoginRequiredMixin, View):
+    def get(self, request):
+        cv = CV.objects.all()
+        context = {
+            'cvs': cv,
+        }
+        return render(request, 'index.html', context=context)
