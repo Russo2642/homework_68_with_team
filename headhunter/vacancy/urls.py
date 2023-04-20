bcd93c53c95b8369ac52aa2abf52ad129391a935
Daniyar Otegen <daniyarotegen@gmail.com>
2023-04-20 19:26:57 +0600

    #24: исправил ссылки

--- headhunter/vacancy/urls.py
+++ headhunter/vacancy/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from vacancy.views.vacancy import VacancyCreateView, VacancyDetailView, VacancyUpdateView, VacancyListView
+from vacancy.views.application import ApplyVacancyView, UserApplicationsView
+
 
 urlpatterns = [
     path('vacancies/', VacancyListView.as_view(), name='vacancies'),
     path('vacancy/create/', VacancyCreateView.as_view(), name='vacancy_create'),
     path('vacancy/<int:pk>/detail/', VacancyDetailView.as_view(), name='vacancy_detail'),
-    path('vacancy/<int:pk>/update/', VacancyUpdateView.as_view(), name='vacancy_update')
+    path('vacancy/<int:pk>/update/', VacancyUpdateView.as_view(), name='vacancy_update'),
+    path('apply_vacancy/<int:vacancy_id>/', ApplyVacancyView.as_view(), name='apply_vacancy'),
+    path('my_applications/', UserApplicationsView.as_view(), name='my_applications'),
 ]
