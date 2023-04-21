a62214202f3ad3faf96735ed16451adfdbb16446
Ilkham Baudinov <ilkhambaudinov@gmail.com>
2023-04-22 02:54:30 +0600

    #10 Добавил Возможность обновления списка вакансий в лч работодателя, прописал url, усовершенствовал модель

--- headhunter/vacancy/urls.py
+++ headhunter/vacancy/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from vacancy.views.vacancy import VacancyCreateView, VacancyDetailView, VacancyUpdateView, VacancyListView, CompanyVacanciesView, VacancyDeleteView
+from vacancy.views.vacancy import (VacancyCreateView,VacancyDetailView,
+                                   VacancyUpdateView, VacancyListView, UpdateListView,
+                                   CompanyVacanciesView, IsPublishView, VacancyDeleteView
+                                   )
 from vacancy.views.application import ApplyVacancyView, UserApplicationsView, ApplicantsView
 from vacancy.views.chat import ChatView, ChatListView
-
-
 
 urlpatterns = [
     path('vacancies/', VacancyListView.as_view(), name='vacancies'),
@@ -12,6 +13,8 @@ urlpatterns = [
     path('vacancy/<int:pk>/update/', VacancyUpdateView.as_view(), name='vacancy_update'),
     path('vacancy/<int:pk>/delete', VacancyDeleteView.as_view(), name='vacancy_delete'),
     path('apply_vacancy/<int:vacancy_id>/', ApplyVacancyView.as_view(), name='apply_vacancy'),
+    path('vacancy/<int:pk>/publishing/', IsPublishView.as_view(), name='vacancy_publish'),
+    path('vacancy/<int:pk>/update_list', UpdateListView.as_view(), name='update_list'),
     path('my_applications/', UserApplicationsView.as_view(), name='my_applications'),
     path('vacancies_company/<int:pk>/', CompanyVacanciesView.as_view(), name='company_vacancies'),
     path('applicants/', ApplicantsView.as_view(), name='applicants'),
